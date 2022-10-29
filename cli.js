3da9556486dc842efd342234b082e95959bf6ac8
Elvin-Liu237 <elvinliu@live.unc.edu>
2022-10-29 15:58:05 -0400

    update

--- cli.js
+++ cli.js
@@ -3,6 +3,7 @@
 import minimist from "minimist"
 import moment from "node-fetch"
 import fetch from "moment-timezone"
+import { AuthRegistrationsCredentialListMappingContext } from "twilio/lib/rest/api/v2010/account/sip/domain/authTypes/authRegistrationsMapping/authRegistrationsCredentialListMapping";
 
 
 var argv = minimist(process.argv.slice(2));
@@ -19,25 +20,56 @@ if (argv.h) {
     
 }
 
+//timezone
 const timezone = moment.tz.guess()
 
 const latitude = argv.n
 const longitude = argv.e
 
-
+if (argv.n) {
+  latitude = args.n;
+}
+if (argv.n) {
+  latitude = args.n;
+}
+if (args.w) {
+  longitude = args.s * -1;
+}
+if (args.e) {
+  longitude 
+}
 // Make a request
 const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m' + 'latitude=' + latitude + '&longitude=' + longitude);
+
+
+
+// Get data from request
 const data = await response.json();
 
+//print data
 if (argv.j) {
     console.log(data);
     process.exit(0);
 
 }
 
+const day = 1;
+
+
+
+
+
+//check if there is precipitation for the day
+ if (data.daily.preciptiation_hous[day] = 0) {
+   console.log("No need to wear galoshes")
+ } else {
+   console.log("Weat your galoshes ")
+ }
+
+
+//print precipitation for selected day
 
 const days = argv.d 
-
 
 if (days == 0) {
   console.log("today.")
